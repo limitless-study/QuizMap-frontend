8ab0eb37a5b2dff83d89d258727fdc339139b836
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2022-04-03 03:38:42 +0900

    add 'back to flashcard' button in mind-map

--- src/pages/MindMapPage.jsx
+++ src/pages/MindMapPage.jsx
@@ -1,8 +1,10 @@
 import { useLayoutEffect } from 'react';
 
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 import { useSelector, useDispatch } from 'react-redux';
+
+import { CgArrowLeftO } from 'react-icons/cg';
 
 import styled from '@emotion/styled';
 
@@ -22,6 +24,27 @@ const Wrapper = styled.div({
   display: 'flex',
 });
 
+const BackButton = styled.div({
+  position: 'fixed',
+  zIndex: '100',
+  right: '0',
+  height: '35px',
+  lineHeight: '35px',
+  backgroundColor: 'white',
+  padding: '0 8px',
+  margin: '20px',
+  borderRadius: '5px',
+  boxShadow: '0 1px 2px 0 rgb(0 0 0 / 20%)',
+  textAlign: 'center',
+  fontSize: '13px',
+  '& a': {
+    display: 'inline-block',
+    '.icon': {
+      verticalAlign: 'middle',
+    },
+  },
+});
+
 export default function MindMapPage({ params }) {
   const { id } = params || useParams();
 
@@ -38,6 +61,13 @@ export default function MindMapPage({ params }) {
   return (
     <Wrapper>
       <SideMenuBar menus={menus} />
+      <BackButton>
+        <Link to={`/cardsets/${id}`}>
+          <CgArrowLeftO className="icon" />
+          {' '}
+          Back to Flashcard
+        </Link>
+      </BackButton>
       <MindMap mindMapCards={mindMapCards} />
     </Wrapper>
   );
