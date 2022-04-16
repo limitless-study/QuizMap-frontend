dc07b6d930e0dc67e6abd59c411bf9c6ca0a081d
Eunseo Kim <kim.eunseo@kakao.com>
2022-04-16 21:41:20 +0900

    studio page의 편집기 카드 input →  textarea로 수정

--- src/components/MainStudio.jsx
+++ src/components/MainStudio.jsx
@@ -10,49 +10,52 @@ const Wrapper = styled.div({
 
 const CreateCardField = styled.div({
   position: 'absolute',
+  display: 'flex',
   top: '50%',
   left: '50%',
-  transform: 'translate(-50%, -50%)',
+  transform: 'translate(-40%, -50%)',
 });
 
-const CreateCard = styled.div({
+const TextArea = styled.textarea({
+  width: '19em',
+  height: '16em',
+  margin: '10px',
   padding: '10px',
-  '& input': {
-    width: '21em',
-    height: '16em',
-    padding: '10px',
-    border: '1px solid lightgray',
-    borderRadius: '10px',
-    boxShadow: 'rgba(0, 0, 0, 0.09) 0px 3px 12px',
-  },
+  border: '1px solid lightgray',
+  borderRadius: '10px',
+  boxShadow: 'rgba(0, 0, 0, 0.09) 0px 3px 12px',
+  resize: 'none',
+  fontSize: '20px',
+  backgroundColor: 'transparent',
+  fontFamily: 'sans-serif',
 });
 
 export default function MainStudio({ currentCard, onChange }) {
   const { question, answer } = currentCard;
 
+  const handleChange = ({ name, value }) => {
+    onChange({ name, value });
+  };
+
   return (
     <Wrapper>
       <CreateCardField>
-        <CreateCard>
-          <InputField
-            inputText={question}
-            labelText="flashcard-question"
-            id="flashcard-question"
-            inputName="question"
-            placeholder="enter your question here"
-            onChange={onChange}
-          />
-        </CreateCard>
-        <CreateCard>
-          <InputField
-            inputText={answer}
-            labelText="flashcard-answer"
-            id="flashcard-answer"
-            inputName="answer"
-            placeholder="enter your answer here"
-            onChange={onChange}
-          />
-        </CreateCard>
+        <TextArea
+          value={question}
+          name="question"
+          id="flashcard-question"
+          inputName="question"
+          placeholder="enter your question here"
+          onChange={(event) => handleChange({ name: 'question', value: event.target.value })}
+        />
+        <TextArea
+          value={answer}
+          name="answer"
+          id="flashcard-answer"
+          inputName="answer"
+          placeholder="enter your answer here"
+          onChange={(event) => handleChange({ name: 'answer', value: event.target.value })}
+        />
       </CreateCardField>
     </Wrapper>
   );
