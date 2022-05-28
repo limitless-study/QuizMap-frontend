043d97d70f48572339a4d723397affaeaa915f36
Eunseo Kim <kim.eunseo@kakao.com>
2022-05-28 20:45:51 +0900

    페이지 새로고침 시 첫 렌더링 될 때 accessToken을 리덕스 스토어 안에 넣기

--- src/store.js
+++ src/store.js
@@ -6,6 +6,25 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducer from './reducer';
 
+import { loadItem } from './services/storage';
+
+import {
+  setToken,
+} from './actions';
+
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+
+function loadUser() {
+  try {
+    const accessToken = loadItem('accessToken');
+    if (accessToken) {
+      store.dispatch(setToken(accessToken));
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+loadUser();
 
 export default store;
