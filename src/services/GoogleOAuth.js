91ace853b858e73fbe300b76cddcd91969c7ec5f
Eunseo Kim <kim.eunseo@kakao.com>
2022-05-29 23:45:58 +0900

    redirect uri를 배포용으로 바꾸기

--- src/services/GoogleOAuth.js
+++ src/services/GoogleOAuth.js
@@ -1,5 +1,6 @@
 const clientId = '485211779781-l1tgqmlf955l76f04dlotmfjtehp21et.apps.googleusercontent.com';
-const redirectUri = 'http://localhost:8080/google/callback';
+// const redirectUri = 'http://localhost:8080/google/callback';
+const redirectUri = 'https://quizmap.co.kr/google/callback';
 const scope = 'https://www.googleapis.com/auth/userinfo.email';
 const GOOGLE_AUTO_URL = `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scope}`;
 
