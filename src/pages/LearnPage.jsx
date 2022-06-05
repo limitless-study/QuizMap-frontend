ebb6d0dea8390da2dbdc633060bc3ec19ff460b1
Eunseo Kim <kim.eunseo@kakao.com>
2022-06-05 19:30:45 +0900

    [fix] React Hook을 top level에서 호출하기 위해 is-else로 NoMoreCards / Loading 띄우는 부분을 Page로 옮김
    - Don’t call Hooks inside loops, conditions, or nested functions. 
    - Instead, always use Hooks at the top level of your React function, before any early returns.

--- src/pages/LearnPage.jsx
+++ src/pages/LearnPage.jsx
@@ -1,14 +1,19 @@
 import { useEffect } from 'react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { useParams } from 'react-router-dom';
 
 import LearnCardsContainer from '../containers/LearnCardsContainer';
+import NoMoreCards from '../components/learn/NoMoreCards';
+
+import Loading from '../components/common/Loading';
 
 import {
   initializeLearnPage,
 } from '../actions';
+
+import { get } from '../utils';
 
 export default function LearnPage({ params }) {
   const { id } = params || useParams();
@@ -18,6 +23,31 @@ export default function LearnPage({ params }) {
   useEffect(() => {
     dispatch(initializeLearnPage(id));
   }, [id]);
+
+  const cards = useSelector(get('cards'));
+  const isLastPage = useSelector(get('isLastPage'));
+
+  if (isLastPage) {
+    return (
+      <NoMoreCards id={id} />
+    );
+  }
+
+  if (cards.length === 0) {
+    return (
+      <div style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+      >
+        <Loading
+          size={80}
+        />
+      </div>
+    );
+  }
 
   return (
     <LearnCardsContainer id={id} />
