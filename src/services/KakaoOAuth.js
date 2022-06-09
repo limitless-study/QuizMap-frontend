a417dc37aae515680d1381538b31e76a07a55ebc
Eunseo Kim <kim.eunseo@kakao.com>
2022-06-10 01:31:08 +0900

    카카오 redirect url 배포용으로 변경

--- src/services/KakaoOAuth.js
+++ src/services/KakaoOAuth.js
@@ -1,6 +1,6 @@
 const CLIENT_ID = 'b84c78d2735d1a81acccdb5fde9ce215';
-const REDIRECT_URI = 'http://localhost:8080/kakao/callback';
-// const REDIRECT_URI = 'https://quizmap.co.kr/kakao/callback';
+// const REDIRECT_URI = 'http://localhost:8080/kakao/callback';
+const REDIRECT_URI = 'https://quizmap.co.kr/kakao/callback';
 
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
